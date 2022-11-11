d7bb9a6af21e3cea198f71dbce56fe7f065b4e77
Simon <simon.tan09@gmail.com>
2022-11-11 11:01:31 -0800

    import track and upvote route

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,14 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 from .models import db, User
+
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.track_routes import track_routes
+from .api.upvote_routes import upvote_routes
+
 from .seeds import seed_commands
+
 from .config import Config
 
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
@@ -65,8 +70,8 @@ def inject_csrf_token(response):
 @app.route('/<path:path>')
 def react_root(path):
     """
-    This route will direct to the public directory in our  
-    react builds in the production environment for favicon 
+    This route will direct to the public directory in our
+    react builds in the production environment for favicon
     or index.html requests
     """
     if path == 'favicon.ico':
@@ -81,7 +86,7 @@ def api_help():
     Returns all API routes and their doc strings
     """
     acceptable_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    route_list = { rule.rule: [[ method for method in rule.methods if method in acceptable_methods ], 
-                    app.view_functions[rule.endpoint].__doc__ ] 
+    route_list = { rule.rule: [[ method for method in rule.methods if method in acceptable_methods ],
+                    app.view_functions[rule.endpoint].__doc__ ]
                     for rule in app.url_map.iter_rules() if rule.endpoint != 'static' }
     return route_list
