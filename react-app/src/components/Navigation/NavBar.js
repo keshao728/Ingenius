b5e46cba4bd94f564f8d1fd6ec1c5ee092a6df83
Kelly <kellyshao728@gmail.com>
2022-11-13 04:48:49 -0800

    Navigation Set Up

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -14,29 +14,12 @@ const NavBar = () => {
   if (sessionUser) {
     sessionLinks = (
       //Signed in user - profile pic and drop down(logout button + view profile)
-      <div class='right-nav' id="navs">
-        <img class='profile-pic' src="https://drive.google.com/uc?export=view&id=1e6AIQpUAr0_HcNJNaptcQAHEdO5aib5k"
+      <div className='session-right-nav' id="navs">
+        <img className='profile-pic' src="https://drive.google.com/uc?export=view&id=1e6AIQpUAr0_HcNJNaptcQAHEdO5aib5k"
           alt="Default Profile"></img>
         {/* ADD A DROP DOWN HERE */}
         <div>
           <LogoutButton />
-        </div>
-      </div>
-    )
-  } else {
-    //Not signed in user - Sign in / Log In
-    sessionLinks = (
-
-      <div>
-        <div>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </div>
-        <div>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
         </div>
       </div>
     )
@@ -45,12 +28,27 @@ const NavBar = () => {
   return (
     <nav>
       <div className='full-navigation'>
-        <div class="mt"></div>
-        <div class='left-nav' id="navs">
+        <div className="empty-placeholder"></div>
+        <div className='site-logo' id="navs">
           <NavLink to='/' exact={true} activeClassName='active'>
             <img id="icon" src={logo} alt="Logo"></img>
           </NavLink>
         </div>
+        <div className='right-nav'>
+          <div>
+            <NavLink to='/sign-up' exact={true} id="nav-buttons" activeClassName='active'>
+              SIGN UP
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to='/login' exact={true} id="nav-buttons" activeClassName='active'>
+              SIGN IN
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      <div>
+        {sessionLinks}
       </div>
     </nav>
   );
