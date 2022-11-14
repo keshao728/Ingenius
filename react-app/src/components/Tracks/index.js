608e3abd1e8b3011e7a486d1a272a417d2e12204
Schaeffer <schaefferahn@gmail.com>
2022-11-14 12:28:19 -0800

    all tracks showing

---
+++ react-app/src/components/Tracks/index.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllTracks } from '../../store/tracks';
+import React from 'react';
+
+
+
+
+export default function AllTracks() {
+    // const params = useParams()
+    const dispatch = useDispatch()
+    // const {trackId} = params
+    const tracks = useSelector(state => state.tracks)
+    console.log("TRACKS", tracks)
+    const allTracks = Object.values(tracks)
+
+    useEffect(() => {
+        dispatch(getAllTracks())
+    }, [dispatch])
+
+    return (
+        <div>
+            {allTracks.map((track) => {
+                return (
+                    <div key={track.id}>
+                        <div>
+                            {track.track_title}
+                        </div>
+
+                        <div>
+                            {track.artist}
+                        </div>
+
+                        <div>
+                            {track.album}
+                        </div>
+
+                        <div>
+                            {track.lyrics}
+                        </div>
+
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
