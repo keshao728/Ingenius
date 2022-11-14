9b359393407ae577f9b9237f3435d941ab52fde0
Simon <simon.tan09@gmail.com>
2022-11-14 11:24:14 -0800

    debug1

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,8 @@ from .models import db, User
 
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
-# from .api.track_routes import track_routes
-# from .api.upvote_routes import upvote_routes
+from .api.track_routes import track_routes
+from .api.upvote_routes import upvote_routes
 
 from .seeds import seed_commands
 
