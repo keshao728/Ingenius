79cd2bcc0b548c7b977619dc6e28633679ddc1bd
Kelly <kellyshao728@gmail.com>
2022-11-13 16:15:39 -0800

    Comment Route - POST

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -16,3 +16,23 @@ def comments():
     #       }
     #   }'
     pass
+
+@comment_routes('/<:artist>-<:song>',methods=["POST"])
+def create_comments():
+    #form = formforcomment()
+    #if form.validate_on_submit():
+    #   data = dbforcomment()
+    #   form.populate_obj(data)
+    #   db.session.add(data)
+    #   db.session.commit()
+    #   return redirect ('/tosomewhere?')
+    #if form.errors:
+    #   return '{
+    #   "message": "Validation Error",
+    #   "statusCode": 400,
+    #   "errors": {
+                #"body": "Comment body text is required"
+                #"statusCode": 404
+    #       }
+    #   }'
+    pass
