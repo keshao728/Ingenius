657fe542c68b05298229703f333c08044ee80f7b
Simon <simon.tan09@gmail.com>
2022-11-14 09:27:45 -0800

    first seed for track

---
+++ app/seeds/tracks.py
@@ -0,0 +1,59 @@
+from ..models import db, Track
+from ..__init__ import app
+
+with app.app_context():
+    track1 = Track(
+        user_id=1,
+        track_title="Skeletons",
+        artist = "Keshi",
+        album = "trilogy",
+        release_date = "2019-07-15",
+        produced_by = "Keshi",
+        lyrics =
+        '''
+        [Verse 1]
+        Stressed out
+        Feelin' lost and I don't know what to do now
+        What I chose ain't really gonna work out
+        Fallin' six feet underneath the floor now
+        Think I'm gonna tap out (Ooh-ahh, ooh-ahh)
+
+        [Pre-Chorus]
+        Feel bad, go to bed
+        Wake up even worse, yeah
+        So sad, in my head
+        Feelin' like a curse
+        I need medicine, medicine, medicine
+        [Chorus]
+        All my skeletons out for the taking (Ooh-ahh)
+        Yeah, I don't even know if I'ma make it
+        I'm afraid of myself and I hate it (Ooh-ahh)
+        All my skeletons out for the taking
+        Somebody take 'em
+
+        [Verse 2]
+        I wanna run away
+        The day to day is taking its toll on me
+        And I'm tearin' at the seams (Ooh-ahh)
+        Throw it all away
+        Give a fuck about what they say
+        I gotta disagree
+        This ain't really fun for me
+
+        [Pre-Chorus]
+        Feel bad, go to bed
+        Wake up even worse, yeah
+        So sad, in my head
+        Feelin' like a curse
+        I need medicine, medicine, medicine
+
+        [Chorus]
+        All my skeletons out for the taking (Ooh-ahh)
+        Yeah, I don't even know if I'ma make it
+        I'm afraid of myself and I hate it (Ooh-ahh)
+        All my skeletons out for the taking
+        Somebody take 'em
+        ''' ,
+        track_art = 'https://t2.genius.com/unsafe/331x331/https%3A%2F%2Fimages.genius.com%2F0c68f4642eadc6d1160d3815cf66ec96.831x831x1.jpg',
+        track_url = 'https://www.youtube.com/watch?v=w_6fWYY6pRw',
+)
