d650c93c8d93948e272299b5ae822588f0f99697
Simon <simon.tan09@gmail.com>
2022-11-14 12:14:56 -0800

    seed annoataion and vote

---
+++ app/seeds/annotations.py
@@ -0,0 +1,50 @@
+from app.model import db, Annotation
+
+def seed_annotations():
+    annotation1 = Annotation(
+        user_id=1,
+        track_id=1,
+        annotation_body='first annotation',
+        startIndex=0,
+        endIndex=10,
+    )
+
+    annotation2 = Annotation(
+        user_id=1,
+        track_id=1,
+        annotation_body='second annotation',
+        startIndex=11,
+        endIndex=20,
+    )
+    annotation3 = Annotation(
+        user_id=1,
+        track_id=2,
+        annotation_body='third annotation',
+        startIndex=5,
+        endIndex=15,
+    )
+    annotation4 = Annotation(
+        user_id=2,
+        track_id=2,
+        annotation_body='fourth annotation',
+        startIndex=16,
+        endIndex=30,
+    )
+    annotation5 = Annotation(
+        user_id=3,
+        track_id=3,
+        annotation_body='fifth annotation',
+        startIndex=25,
+        endIndex=35,
+    )
+
+    db.session.add(annotation1)
+    db.session.add(annotation2)
+    db.session.add(annotation3)
+    db.session.add(annotation4)
+    db.session.add(annotation5)
+    db.session.commit()
+
+def undo_annotations():
+    db.session.execute('TRUNCATE annotations RESTART IDENTITY CASCADE;')
+    db.session.commit()
---
+++ app/seeds/votes.py
@@ -0,0 +1,39 @@
+from app.models import db, Vote
+
+def seed_vote():
+    vote1 = Vote(
+        annotation_id=1,
+        user_id=1,
+        vote=1,
+    )
+    vote2 = Vote(
+        annotation_id=1,
+        user_id=2,
+        vote=-1,
+    )
+    vote3 = Vote(
+        annotation_id=1,
+        user_id=3,
+        vote=1,
+    )
+    vote4 = Vote(
+        annotation_id=2,
+        user_id=1,
+        vote=1,
+    )
+    vote5 = Vote(
+        annotation_id=2,
+        user_id=2,
+        vote=1,
+    )
+
+    db.session.add(vote1)
+    db.session.add(vote2)
+    db.session.add(vote3)
+    db.session.add(vote4)
+    db.session.add(vote5)
+    db.session.commit()
+
+    def undo_vote():
+        db.session.execute('TRUNCATE votes RESTART IDENTITY CASCADE;')
+        db.session.commit()
