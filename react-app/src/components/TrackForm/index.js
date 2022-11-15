8f5193ab7805d23ad60ce84a274cd5bae3ca7214
Kelly <kellyshao728@gmail.com>
2022-11-15 15:44:02 -0800

    Add a Song CSS

--- react-app/src/components/TrackForm/index.js
+++ react-app/src/components/TrackForm/index.js
@@ -2,169 +2,163 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { createTrack } from '../../store/tracks';
 import { useHistory } from 'react-router-dom';
+import "./TrackForm.css"
 
 export default function CreateTrack() {
-    const dispatch = useDispatch()
-    const history = useHistory()
+  const dispatch = useDispatch()
+  const history = useHistory()
 
-    const [trackTitle, setTrackTitle] = useState('')
-    const [artist, setArtist] = useState('')
-    const [album, setAlbum] = useState('')
-    const [releaseDate, setReleaseDate] = useState('')
-    const [producedBy, setProducedBy] = useState('')
-    const [lyrics, setLyrics] = useState('')
-    const [trackArt, setTrackArt] = useState('')
-    const [trackUrl, setTrackUrl] = useState('')
-    const [errors, setErrors] = useState([])
-
-
-
-    // let validate = () => {
-    //     let validationErrors = []
+  const [trackTitle, setTrackTitle] = useState('')
+  const [artist, setArtist] = useState('')
+  const [album, setAlbum] = useState('')
+  const [releaseDate, setReleaseDate] = useState('')
+  const [producedBy, setProducedBy] = useState('')
+  const [lyrics, setLyrics] = useState('')
+  const [trackArt, setTrackArt] = useState('')
+  const [trackUrl, setTrackUrl] = useState('')
+  const [errors, setErrors] = useState([])
 
 
-    //     setErrors(validationErrors)
 
-    //     return validationErrors
-    // }
+  // let validate = () => {
+  //     let validationErrors = []
 
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        if (!errors.length) {
-            setErrors([])
+  //     setErrors(validationErrors)
 
-            const track = {
-                track_title: trackTitle,
-                artist,
-                album,
-                release_date: releaseDate,
-                produced_by: producedBy,
-                lyrics,
-                track_art: trackArt,
-                track_url: trackUrl
-            }
+  //     return validationErrors
+  // }
 
-            let createdTrack = await dispatch(createTrack(track)).catch(async (res) => {
-                const data = await res.json();
-                if (data && data.errors) setErrors(data.errors)
-            })
-            if (createdTrack) history.push(`/tracks/${createdTrack.id}`)
-        }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!errors.length) {
+      setErrors([])
+
+      const track = {
+        track_title: trackTitle,
+        artist,
+        album,
+        release_date: releaseDate,
+        produced_by: producedBy,
+        lyrics,
+        track_art: trackArt,
+        track_url: trackUrl
+      }
+
+      let createdTrack = await dispatch(createTrack(track)).catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors)
+      })
+      if (createdTrack) history.push(`/tracks/${createdTrack.id}`)
     }
+  }
 
-    return (
-        <>
+  return (
+    <div className='full-screen-add-song'>
+      <form id="trackForm" onSubmit={handleSubmit}>
+        <div className='add-song-wrapper'>
+          <div className='add-song-child'>
+            <div className='add-song-message'>Add Song</div>
+            <div className='add-song-primary'>
+              <div className='add-song-primart-text'>Primary info</div>
+              <div className='add-by-title-album'>
+
+
+                <div>
+                  <label>BY</label>
+                  <input
+                    className='add-song-input-box'
+                    type="text"
+                    value={artist}
+                    placeholder="The primary artist, author, creator, etc."
+                    onChange={(e) => setArtist(e.target.value)} />
+                </div>
+
+                <div>
+                  <label>TITLE</label>
+                  <input
+                    type="text"
+                    className='add-song-input-box'
+                    value={trackTitle}
+                    placeholder="Track Title"
+                    required
+                    onChange={(e) => setTrackTitle(e.target.value)} />
+                </div>
+
+
+                <div>
+                  <label>ALBUM</label>
+                  <input
+                    className='add-song-input-box'
+                    type="text"
+                    value={album}
+                    placeholder="Album"
+                    onChange={(e) => setAlbum(e.target.value)} />
+                </div>
+              </div>
+              <div>
+                <label>LYRICS</label>
+                <textarea
+                  type="text"
+                  className='add-song-input-box'
+                  value={lyrics}
+                  onChange={(e) => setLyrics(e.target.value)}
+                >
+                </textarea>
+              </div>
+            </div>
+
+
             <div>
-                <h1>
-                    Create a Track
-                </h1>
+              <label>RELEASED DATE</label>
+              <input
+                type="date"
+                className='add-song-input-box'
+                value={releaseDate}
+                placeholder="Release Date"
+                onChange={(e) => setReleaseDate(e.target.value)} />
             </div>
 
             <div>
-                <form id="trackForm" onSubmit={handleSubmit}>
-
-                    <div>
-                        <label>
-                            Title
-                        </label>
-                        <input
-                            type="text"
-                            value={trackTitle}
-                            placeholder="Track Title"
-                            required
-                            onChange={(e) => setTrackTitle(e.target.value)} />
-                    </div>
-
-                    <div>
-                        <label>
-                            Artist
-                        </label>
-                        <input
-                            type="text"
-                            value={artist}
-                            placeholder="Artist"
-                            onChange={(e) => setArtist(e.target.value)} />
-                    </div>
-
-                    <div>
-                        <label>
-                            Album
-                        </label>
-                        <input
-                            type="text"
-                            value={album}
-                            placeholder="Album"
-                            onChange={(e) => setAlbum(e.target.value)} />
-                    </div>
-
-                    <div>
-                        <label>
-                            Release Date
-                        </label>
-                        <input
-                            type="date"
-                            value={releaseDate}
-                            placeholder="Release Date"
-                            onChange={(e) => setReleaseDate(e.target.value)} />
-                    </div>
-
-                    <div>
-                        <label>
-                            Producer
-                        </label>
-                        <input
-                            type="text"
-                            value={producedBy}
-                            placeholder="Producer"
-                            onChange={(e) => setProducedBy(e.target.value)} />
-                    </div>
-
-
-
-                    <div>
-                        <label>
-                            Track Art
-                        </label>
-                        <input
-                            type="text"
-                            value={trackArt}
-                            placeholder="Track Art"
-                            onChange={(e) => setTrackArt(e.target.value)} />
-                    </div>
-
-                    <div>
-                        <label>
-                            Track Url
-                        </label>
-                        <input
-                            type="text"
-                            value={trackUrl}
-                            placeholder="Track Url"
-                            onChange={(e) => setTrackUrl(e.target.value)} />
-                    </div>
-
-                    <div>
-                        <label>
-                            Lyrics
-                        </label>
-                        {/* <input
-                            type="textarea"
-                            value={lyrics}
-                            placeholder="Lyrics"
-                            required
-                            onChange={(e) => setLyrics(e.target.value)} /> */}
-
-                            <textarea value={lyrics} onChange={(e) => setLyrics(e.target.value)} id="trackForm" cols="50" rows='30' name="trackFormLyrics"></textarea>
-                    </div>
-
-                    <div>
-                        <button type="submit">Create Track</button>
-                    </div>
-
-                </form>
+              <label>PRODUCER</label>
+              <input
+                type="text"
+                value={producedBy}
+                className='add-song-input-box'
+                placeholder="Producer"
+                onChange={(e) => setProducedBy(e.target.value)} />
             </div>
-        </>
-    )
+
+
+
+            <div>
+              <label>ALBUM COVER</label>
+              <input
+                type="text"
+                className='add-song-input-box'
+                value={trackArt}
+                placeholder="Track Art"
+                onChange={(e) => setTrackArt(e.target.value)} />
+            </div>
+
+            <div>
+              <label>YOUTUBE URL</label>
+              <input
+                type="text"
+                value={trackUrl}
+                className='add-song-input-box'
+                placeholder="Track Url"
+                onChange={(e) => setTrackUrl(e.target.value)} />
+            </div>
+
+          </div>
+          <div>
+            <button type="submit">Create Track</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
 
 }
