9336694e4b1eaa6d52bf75eca5805b4a7ad4c123
Schaeffer <schaefferahn@gmail.com>
2022-11-15 10:33:53 -0800

    edit track working? still needs validations

--- react-app/src/components/TrackInfo/index.js
+++ react-app/src/components/TrackInfo/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams, useHistory, Link, NavLink } from 'react-router-dom';
 import { getOneTrack } from '../../store/tracks';
 import EditTrackModal from '../TrackEditForm/index';
+import { actionResetTrack } from '../../store/tracks';
 
 import React from 'react';
 
@@ -13,14 +14,17 @@ export default function TrackInfo() {
     const { trackId } = useParams()
     const dispatch = useDispatch()
     const track = useSelector(state => state.tracks)
+    const user = useSelector(state => state.session.user)
 
     useEffect(() => {
         dispatch(getOneTrack(trackId))
-    }, [dispatch, trackId])
+
+        return () => dispatch(actionResetTrack())
+    }, [dispatch, user, trackId])
 
     return (
         <div>
-            <EditTrackModal />
+            {user.id === track.user_id && <EditTrackModal />}
 
             <div>
                 Track Title: {track.track_title}
