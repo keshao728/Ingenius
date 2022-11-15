f0e52fb51a99955e959a53d91606b23e1fc120ef
andrew <a.kimswk@gmail.com>
2022-11-14 22:35:36 -0800

    bug fixed

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -1,24 +1,30 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom'
-import { getUserAnnotations } from '../../store/annotations';
+import { getUserAnnotations } from '../../store/annotations'
 import React from 'react';
 
 
 const UserAnnotations = () => {
-  const annotations = useSelector(state => state.annotations.getUserAnnotations)
+  const getAnnotations = useSelector(state => state.annotations.getUserAnnotations)
 
+  console.log('GETANNOTATIONS', getAnnotations)
   const dispatch = useDispatch()
   const { userId } = useParams()
 
+  const [isLoaded, setIsLoaded ] = useState(false)
+
+  const annotations = Object.values(getAnnotations)
+
   useEffect(() => {
     dispatch(getUserAnnotations(userId))
-  }, [dispatch])
+    .then(() => setIsLoaded(true))
+  }, [dispatch, userId])
 
   if (!annotations) {
     return null
   } else {
-    return (
+    return isLoaded (
       <div>
         {Object.values(annotations).map(annotation => {
           <div>{annotation.id}</div>
