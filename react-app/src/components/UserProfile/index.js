580ad0743ed8ba5da4227159ab602ff50129ba4e
andrew <a.kimswk@gmail.com>
2022-11-16 02:25:00 -0800

    userpage right side css almost fin

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -34,19 +34,30 @@ const UserAnnotations = () => {
       <div id='pp-annotations-outer-container' key={annotation.id}>
         <div id='pp-annotation-created-at'> {annotation.created_at}</div>
         <div id='pp-annotation-inner-container'>
-          <div id='pp-annotation-song'>
-            <div id='pp-annotation-song-title'> {annotation.track.track_title}</div>
-            <div id='pp-annotation-song-artist'>{annotation.track.artist}</div>
+          <div id='pp-song-info'>
+            <img id='pp-album-photo' src={annotation.track.track_art} test />
+            <div id='pp-title-artist'>
+              <div id='pp-annotation-song-title'> {annotation.track.track_title}</div>
+              <div id='pp-annotation-song-artist'>{annotation.track.artist}</div>
+            </div>
           </div>
-          <div id='pp-annotation-lyric'>
-            lyrics
+          <div id='pp-annotation-lyric-container'>
+            <div id='pp-annotation-lyric'>
+              lyrics
+            </div>
+            <div>
+
+            </div>
           </div>
           <div id='pp-annotation-inner-content'>
-            <div>{annotation.user.username}</div>
-            <div>{annotation.annotation_body}</div>
+            <div id='pp-annotation-username-icon-container'>
+            <img id='pp-annotation-username-icon' src={'https://64.media.tumblr.com/eca7c1a0c4df7688d52cf781e53142d1/3e3c47d5fa9f904a-71/s540x810/1ee4b3b16a25a49c4f61326f895cbf341088041e.jpg'}/>
+            <div id='pp-annotation-username'>{annotation.user.username}</div>
+            </div>
+            <div id='pp-annotation-annotation-body'>{annotation.annotation_body}</div>
             <div id='pp-annotation-delete-edit'>
-              <div><button onClick={() => dispatch(deleteAnnotation(annotation.id))}>DELETE</button></div>
-              <div><button onClick={() => dispatch(editAnnotation(annotation.id))}>EDIT</button></div>
+              <button id='pp-annotation-edit'onClick={() => dispatch(editAnnotation(annotation.id))}>Edit</button>
+              <button id='pp-annotation-delete' onClick={() => dispatch(deleteAnnotation(annotation.id))}>Delete</button>
             </div>
             <div id='pp-annotation-upvote'>Upvote {annotation.vote_count}</div>
           </div>
