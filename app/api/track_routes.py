3bb2553eacbf2ea2a195cc7d1a1128a7549a3c9f
Schaeffer <schaefferahn@gmail.com>
2022-11-16 08:54:44 -0800

    delete track working. on cascade added.

--- app/api/track_routes.py
+++ app/api/track_routes.py
@@ -53,6 +53,7 @@ def create_track():
 
     if form.validate_on_submit():
         track = Track(
+            user_id = current_user.id,
             track_title=form.track_title.data,
             artist=form.artist.data,
             album=form.album.data,
