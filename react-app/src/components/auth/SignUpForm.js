146705541ad20ef91597e83f68b9942aef269bc5
Simon <simon.tan09@gmail.com>
2022-11-19 19:56:07 -0800

    reseed data for track link

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -42,14 +42,18 @@ const SignUpForm = () => {
   // function isValidEmail(email) {
   //   return /\S+@\S+\.\S+/.test(email);
   // }
+  function isEmpty(str) {
+    return !str.trim().length
+  }
 
   useEffect(async() => {
     const err = [];
-    if(!username) err.push('Please provide a username');
+    if(!username || isEmpty(username)) err.push('Please provide a username');
     if(username.length > 15) err.push('Username must be less than 15 characters');
     if(username.length < 3) err.push('Username must be at least 3 characters');
-    if(!email) err.push('Please provide an email');
+    if(!email || isEmpty(email)) err.push('Please provide an email');
     if(!email.toLowerCase().match(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,63})$/)) err.push('Please provide a valid email');
+    if(isEmpty(password)) err.push('Please provide a password');
     if (password !== repeatPassword)err.push('Passwords must match')
     if (password.length < 6) err.push('Password must be at least 6 characters')
     if (password.length > 20) err.push('Password length must not exceed 20 characters')
