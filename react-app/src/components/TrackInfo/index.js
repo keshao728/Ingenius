6c1b78f29dd4b634492dd93f4309355e92a84df7
Schaeffer <schaefferahn@gmail.com>
2022-11-20 15:12:42 -0800

    Maybe fixed error where document.getelement is bugging

--- react-app/src/components/TrackInfo/index.js
+++ react-app/src/components/TrackInfo/index.js
@@ -230,11 +230,11 @@ export default function TrackInfo() {
     if (annotations) {
       for (let annotation of annotations) {
         // console.log(annotation.span_ids.split(','))
-        annotation.span_ids.split(',').map(anno =>
-          document.getElementById(anno).classList.add('annotated')
+        annotation?.span_ids?.split(',')?.map(anno =>
+          document?.getElementById(anno)?.classList.add('annotated')
         )
-        annotation.span_ids.split(',').map(anno =>
-          document.getElementById(anno).addEventListener('click', (e) => { setShowAnnotation(true); getAnno(e) })
+        annotation?.span_ids?.split(',')?.map(anno =>
+          document?.getElementById(anno)?.addEventListener('click', (e) => { setShowAnnotation(true); getAnno(e) })
         )
 
 
